3dbe05017bc0273487e10bd655243e585c4cf1c6
BlehCJHunter <51364755+BlehCJHunter@users.noreply.github.com>
2019-09-27 19:29:51 +1000

    Add files via upload

---
+++ src/tetristestground/TetrisBlock.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tetristestground;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Arrays;
+import orion.number.Vector2I;
+
+/**
+ *
+ * @author Orion
+ */
+public class TetrisBlock {
+
+    private final Vector2I[] area;
+    private final Color colour = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
+    private int pattern;
+
+    public TetrisBlock(Vector2I tile1, Vector2I tile2, Vector2I tile3, Vector2I tile4, int pattern) {
+        area = new Vector2I[4];
+        area[0] = tile1;
+        area[1] = tile2;
+        area[2] = tile3;
+        area[3] = tile4;
+        this.pattern = pattern;
+        for (Vector2I tile : area) {
+            System.out.println(Arrays.toString(tile.getPos()));
+        }
+    }
+
+    public TetrisBlock(Vector2I[] area, int pattern) {
+        this.area = area;
+    }
+
+    public void drawSelf(Graphics g, int scaleMultiplier) {
+        g.setColor(colour);
+        for (Vector2I i : area) {
+            g.fillRect(i.getX() * scaleMultiplier, i.getY() * scaleMultiplier, scaleMultiplier, scaleMultiplier);
+        }
+    }
+
+    public boolean intersects(TetrisBlock other) {
+        Vector2I[] otherArea = other.getArea();
+        boolean result = false;
+        for (Vector2I otherTile : otherArea) {
+            for (Vector2I tile : area) {
+                result = (tile.equals(otherTile) || result);
+            }
+        }
+        return result;
+    }
+
+    public Vector2I[] getArea() {
+        return area;
+    }
+
+    public TetrisBlock moveTest(Vector2I delta) {
+        Vector2I[] holder = new Vector2I[area.length];
+        for (int i = 0; i < area.length; i++) {
+            holder[i] = area[i].add(delta);
+        }
+
+        return new TetrisBlock(holder, pattern);
+    }
+
+    public void move(Vector2I delta) {
+        for (int i = 0; i < area.length; i++) {
+            area[i] = area[i].add(delta);
+        }
+    }
+
+    public boolean boundedMove(Vector2I delta, int xBounds, int yBounds) {
+        Vector2I[] holder = new Vector2I[area.length];
+        boolean invalid = false;
+        for (int i = 0; i < area.length; i++) {
+            holder[i] = area[i].add(delta);
+            invalid = (holder[i].getX() >= xBounds) || (holder[i].getY() >= yBounds) || invalid;
+        }
+        if (!invalid) {
+            for (int i = 0; i < area.length; i++) {
+                area[i] = holder[i];
+            }
+        }
+        return !invalid;
+    }
+}
