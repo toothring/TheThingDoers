1d095e281c3e56293f53b589c6948f50dbad8c48
toothring <51364698+toothring@users.noreply.github.com>
2019-09-27 18:10:32 +1000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,18 @@ public class Main{
 	private Tetris tr;
 	
 	public Main() {
-		Scanner sc=new Scanner(System.in);
 		char menuSelect;
 		int players=1;
 		
 		do {
 			System.out.print("Welcome to Tetsaw\n"
 					+ "Please make a selection\n\n"
-					+ "A - One Player\n"
-					+ "B - Two Players\n"
+					+ "A - One Player (MVF)\n"
+					+ "B - Two Players (EF)\n"
 					+ "C - Settings\n"
 					+ "Q - Quit Game\n\n"
 					+ "Enter your choice: ");
-			menuSelect=sc.nextLine().toUpperCase().strip().charAt(0);
+			menuSelect=getString().toUpperCase().strip().charAt(0);
 			if(menuSelect=='B') {
 				players=2;
 				menuSelect='A';
@@ -29,22 +28,25 @@ public class Main{
 							+ "B - Tetris\n"
 							+ "Q - Go back\n\n"
 							+ "Enter your choice: ");
-					menuSelect=sc.nextLine().toUpperCase().strip().charAt(0);
+					menuSelect=getString().toUpperCase().strip().charAt(0);
 					if (menuSelect== 'A') {
 						do {
 							System.out.print("Select a mode:\n\n"
 									+ "A - Easy\n"
 									+ "B - Normal\n"
 									+ "C - Hard\n"
+									+ "D - Custom\n"
 									+ "Q - Go back\n\n"
 									+ "Enter your choice: ");
-							menuSelect=sc.nextLine().toUpperCase().strip().charAt(0);
+							menuSelect=getString().toUpperCase().strip().charAt(0);
 							if (menuSelect == 'A')
 								this.ts=new Tetsaw("easy",players);
 							else if(menuSelect =='B')
 								this.ts=new Tetsaw("normal",players);
 							else if(menuSelect == 'C')
 								this.ts=new Tetsaw("hard",players);
+							else if(menuSelect == 'D')
+								this.ts=new Tetsaw("custom",players);
 							else
 								if(menuSelect != 'Q')
 									System.out.println("Invalid input. Please try again.\n\nEnter your choice: ");
@@ -60,19 +62,70 @@ public class Main{
 			} else if(menuSelect == 'C') {
 				do {
 					System.out.print("Select what you want to change:\n\n"
-							+ "A - Graphics\n"
-							+ "B - Sound\n"
-							+ "C - Accessability\n"
+							+ "A - Graphics (MVF)\n"
+							+ "B - Sound (MVF)\n"
+							+ "C - Accessability (EF)\n"
 							+ "Q - Quit\n\n"
 							+ "Enter your choice: ");
-					menuSelect=sc.nextLine().toUpperCase().strip().charAt(0);
-					if(menuSelect == 'A')
-						graphics();
-					else if(menuSelect == 'B')
-						sound();
-					else if(menuSelect == 'C')
-						accessibility();
-					else {
+					menuSelect=getString().toUpperCase().strip().charAt(0);
+					if(menuSelect == 'A') {
+						
+						do {
+							System.out.print("Select a mode:\n\n"
+									+ "A - Resolution (MVF)\n"
+									+ "B - FPS (MVF)\n"
+									+ "C - Full-Screen Mode (MVF)\n"
+									+ "Q - Go back\n\n"
+									+ "Enter your choice: ");
+							menuSelect=getString().toUpperCase().strip().charAt(0);
+							if (menuSelect == 'A')
+								graphics("Resolution");
+							else if(menuSelect =='B')
+								graphics("FPS");
+							else if(menuSelect == 'C')
+								graphics("Full-Screen Mode");
+							else if(menuSelect == 'D')
+								this.ts=new Tetsaw("custom",players);
+							else
+								if(menuSelect != 'Q')
+									System.out.println("Invalid input. Please try again.\n\nEnter your choice: ");
+						} while (menuSelect != 'Q');
+						menuSelect='\u0000';
+					} else if(menuSelect == 'B')  {
+						do {
+							System.out.print("Select a mode:\n\n"
+									+ "A - Background Music Volume (MVF)\n"
+									+ "B - Sound Effects Volume (MVF)\n"
+									+ "Q - Go back\n\n"
+									+ "Enter your choice: ");
+							menuSelect=getString().toUpperCase().strip().charAt(0);
+							if (menuSelect == 'A')
+								sound("Background Music");
+							else if(menuSelect =='B')
+								sound("Sound Effects");
+							else
+								if(menuSelect != 'Q')
+									System.out.println("Invalid input. Please try again.\n\nEnter your choice: ");
+						} while (menuSelect != 'Q');
+						menuSelect='\u0000';
+					} else if(menuSelect == 'C') {
+						do {
+							System.out.print("Select a mode:\n\n"
+									+ "A - Game Controls (EF)\n"
+									+ "B - Colour-blind Mode (EF)\n"
+									+ "Q - Go back\n\n"
+									+ "Enter your choice: ");
+							menuSelect=getString().toUpperCase().strip().charAt(0);
+							if (menuSelect == 'A')
+								accessibility("Game Controls");
+							else if(menuSelect =='B')
+								accessibility("Colour-blind Mode");
+							else
+								if(menuSelect != 'Q')
+									System.out.println("Invalid input. Please try again.\n\nEnter your choice: ");
+						} while (menuSelect != 'Q');
+						menuSelect='\u0000';
+					} else {
 						if(menuSelect != 'Q')
 							System.out.println("Invalid input. Please try again.\n\nEnter your choice: ");
 					}
@@ -87,18 +140,18 @@ public class Main{
 		} while (menuSelect != 'Q');
 	}
 	
-	public char graphics() {
-		System.out.println("Let's change those graphics settings!\nQ to exit");
+	public char graphics(String setting) {
+		System.out.println(setting + " were WAY off! I'm glad you're changing them.\nQ to exit");
 		return getString().toUpperCase().strip().charAt(0);
 	}
 	
-	public char sound() {
-		System.out.println("Let's change those sound settings!\nQ to exit");
+	public char sound(String setting) {
+		System.out.println("Don't change the " + setting + " too much.\nBrendan did that and how he can't hear high notes!\nQ to exit");
 		return getString().toUpperCase().strip().charAt(0);
 	}
 	
-	public char accessibility() {
-		System.out.println("We love that you're colour blind!\nQ to exit");
+	public char accessibility(String setting) {
+		System.out.println(setting + " engaged!\nQ to disengage!");
 		return getString().toUpperCase().strip().charAt(0);
 	}
 	
