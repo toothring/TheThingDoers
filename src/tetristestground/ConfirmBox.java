5b17b6f544a96e9ca2ccf0604d5cc408400177b3
brendan6780 <51254866+brendan6780@users.noreply.github.com>
2019-10-21 16:33:41 +1000

    Added 'quit' option and confirmation box.

---
+++ src/tetristestground/ConfirmBox.java
@@ -0,0 +1,46 @@
+package tetristestground;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class ConfirmBox {
+
+    static boolean answer;
+
+    public static Boolean display(String title, String message) {
+
+        Stage window = new Stage();
+
+        //Block events to other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+
+        Label label = new Label();
+        label.setText(message);
+        Button yes = new Button("Yes");
+        yes.setOnAction(e -> {
+            answer = true;
+            window.close();
+        });
+        Button no = new Button("No");
+        no.setOnAction(e -> {
+            answer = false;
+            window.close();
+        });
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, yes, no);
+        layout.setAlignment(Pos.CENTER);
+
+        //Display window and wait for it to be closed before returning
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+        return answer;
+    }
+
+}
