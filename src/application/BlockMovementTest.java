ee4995ccf219bf230110f0205ed46607f0b8ce4a
toothring <51364698+toothring@users.noreply.github.com>
2019-10-23 14:11:29 +1100

    commented out code + small changes

--- src/application/BlockMovementTest.java
+++ src/application/BlockMovementTest.java
@@ -1,3 +1,9 @@
+/*
+ * Base code was modified from:
+ * Almas Baimagambetov, JavaFX Game: Space Invaders (for Beginners),
+ * viewed 23 October 2019, <https://www.youtube.com/watch?v=FVo1fm52hz0&t=>
+ */
+
 package application;
 
 import javafx.animation.AnimationTimer;
@@ -12,12 +18,16 @@ import javafx.stage.Stage;
 public class BlockMovementTest extends Application {
 
 	private Pane root = new Pane();
+	//double t is the timer (starts at 0)
 	private double t = 0;
-	private Block player = new Block(200, 0, 40, 40, "player", Color.GREEN);
+	//you are the block
+	private Block player = new Block(200, 0, 40, 40, "player", Color.BLACK); 
 	
 	private Parent createContent() {
+		//board size. 10 accross and 10 down based on 40px block
 		root.setPrefSize(440, 840);
 		root.getChildren().add(player);
+		//creates the timer
 		AnimationTimer timer = new AnimationTimer() {
 			@Override
 			public void handle(long now) {
@@ -29,9 +39,12 @@ public class BlockMovementTest extends Application {
 	}
 	
 	private void tickDown() {
+		// every tick == 0.016s
 		t += 0.016;
+		// when tick reaches 1 second, the block moves down my 40px
 		if (t > 1) {
 			player.moveDown();
+			// tick is reset ready for next round
 			t = 0;
 		}
 	}
@@ -40,6 +53,7 @@ public class BlockMovementTest extends Application {
 	public void start(Stage stage) {
 		Scene scene = new Scene(createContent());
 		
+		//This was taken from a YouTube video. I don't know anything about Lambda
 		scene.setOnKeyPressed(e -> {
 			switch (e.getCode()) {
 				case LEFT:
@@ -55,17 +69,13 @@ public class BlockMovementTest extends Application {
 	}
 	
 	private static class Block extends Rectangle {
-		boolean dead = false;
-		final String type;
-		
+		// how the block will move
 		Block(int x, int y, int w, int h, String type, Color color) {
 			super(w, h, color);
-			
-			this.type = type;
 			setTranslateX(x);
 			setTranslateY(y);
 		}
-		
+		// key events are each a 40px movement
 		void moveLeft() {
 			setTranslateX(getTranslateX() - 40);
 		}
