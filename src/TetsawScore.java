ed342e07683298ff21329e2f72c31194bba0f14c
ZaneFox1 <48506137+ZaneFox1@users.noreply.github.com>
2019-10-29 17:52:21 +1100

    Score test

--- src/TetsawScore.java
+++ src/TetsawScore.java
@@ -1,17 +1,37 @@
 public class TetsawScore {
 
     public static void main(String[] args) {
-        boolean gameOver = true; // is game over
+
+        // Below variables are to be collected from game actions. Example numbers entered not actual numbers.
+
+        // Tetsaw score variables
+        boolean isTetsaw = true; // is the completed game Tetsaw?
+        boolean isTetris = true; // is game being played Tetris?
+        boolean gameOverTetsaw = true; // is game over
         int puzzleCompleteScore = 3; // 1 = easy 1000 points, 2 = medium 1500 points, 3 = hard 2000 points.
         int correctPiecePlacement = 23; // Amount of pieces that have been placed in the correct position.
         int livesLeft = 3; // Amount of lives left at end of game
         int timeLeft = 20; // Amount if time left in countdown after game ends
 
-        int finalScore = calculateScore(gameOver, puzzleCompleteScore, correctPiecePlacement, livesLeft, timeLeft);
-        System.out.println("Final score = " + finalScore);
+        // Tetris score variables
+        boolean rowDestruction = true; // One tick has passed and score has happened.
+        int destructionAmount = 3; // 1 = 130, 2 = 320 points, 3 = 524 points, 4 = 828 points, 5 = 1204 points
+
+
+
+        if(isTetsaw) {
+            int finalScore = calculateTetsawScore(gameOverTetsaw, puzzleCompleteScore, correctPiecePlacement, livesLeft, timeLeft);
+            System.out.println("Final score = " + finalScore);
+        }
+        else if(isTetris) {
+            int currentScore = calculateTetrisScore(rowDestruction);
+        }
+
     }
 
-    public static int calculateScore(boolean gameOver, int puzzleCompleteScore, int correctPiecePlacement, int livesLeft, int timeLeft) {
+
+
+    public static int calculateTetsawScore(boolean gameOver, int puzzleCompleteScore, int correctPiecePlacement, int livesLeft, int timeLeft) {
 
         int levelScore = 0;
         int blockPlacePoints = 0;
@@ -34,12 +54,23 @@ public class TetsawScore {
         // Final Score Calculation
         if(gameOver) {
             int finalScore = (levelScore * livesLeft) + (blockPlacePoints * correctPiecePlacement);
-            System.out.println(finalScore);
+            return finalScore;
         }
+        return -1;
+    }
+
+    public static int calculateTetrisScore(boolean scoreHappen) {
+
+        int rows1 = 130;
+        int rows2 = 320;
+        int rows3 = 524;
+        int rows4 = 828;
+        int tFlip = 1204;
 
 
 
 
         return -1;
     }
+
 }
