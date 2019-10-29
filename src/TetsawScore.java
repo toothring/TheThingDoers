b476d8645501e271dc7ade7192ada719b9998203
ZaneFox1 <48506137+ZaneFox1@users.noreply.github.com>
2019-10-29 16:52:16 +1100

    Score test

---
+++ src/TetsawScore.java
@@ -0,0 +1,40 @@
+public class TetsawScore {
+
+    public static void main(String[] args) {
+        boolean gameOver = true; // is game over
+        int puzzleCompleteScore = 3; // 1 = easy 1000 points, 2 = medium 1500 points, 3 = hard 2000 points.
+        int correctPiecePlacement = 23; // Amount of pieces that have been placed in the correct position.
+        int livesLeft = 3; // Amount of lives left at end of game
+        int timeLeft = 20; // Amount if time left in countdown after game ends
+
+        int finalScore = calculateScore(gameOver, puzzleCompleteScore, correctPiecePlacement, livesLeft, timeLeft);
+        System.out.println("Final score = " + finalScore);
+    }
+
+    public static int calculateScore(boolean gameOver, int puzzleCompleteScore, int correctPiecePlacement, int livesLeft, int timeLeft) {
+
+        int levelScore = 0;
+
+
+        // Converting set level to point values
+        if(puzzleCompleteScore == 1) {
+            levelScore = 1000;
+        }
+        else if(puzzleCompleteScore == 2) {
+            levelScore = 1500;
+        }
+        else if(puzzleCompleteScore == 3) {
+            levelScore = 2000;
+        }
+
+        if(gameOver) {
+            int finalScore = (levelScore * livesLeft);
+            System.out.println(finalScore);
+        }
+
+
+
+
+        return -1;
+    }
+}
