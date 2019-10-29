e87a29f95cff4ee7c699c5d75cc3c567296d53fe
ZaneFox1 <48506137+ZaneFox1@users.noreply.github.com>
2019-10-29 17:02:57 +1100

    Score test

--- src/TetsawScore.java
+++ src/TetsawScore.java
@@ -14,21 +14,26 @@ public class TetsawScore {
     public static int calculateScore(boolean gameOver, int puzzleCompleteScore, int correctPiecePlacement, int livesLeft, int timeLeft) {
 
         int levelScore = 0;
+        int blockPlacePoints = 0;
 
 
         // Converting set level to point values
         if(puzzleCompleteScore == 1) {
             levelScore = 1000;
+            blockPlacePoints = 10;
         }
         else if(puzzleCompleteScore == 2) {
             levelScore = 1500;
+            blockPlacePoints = 15;
         }
         else if(puzzleCompleteScore == 3) {
             levelScore = 2000;
+            blockPlacePoints = 20;
         }
 
+        // Final Score Calculation
         if(gameOver) {
-            int finalScore = (levelScore * livesLeft);
+            int finalScore = (levelScore * livesLeft) + (blockPlacePoints * correctPiecePlacement);
             System.out.println(finalScore);
         }
 
