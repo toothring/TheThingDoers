45fce408007124ed0011c586b816b50a2781a193
toothring <51364698+toothring@users.noreply.github.com>
2019-11-05 14:45:21 +1100

    Commit DAMMIT!

--- src/testjfxapp/TetrisBlock.java
+++ src/testjfxapp/TetrisBlock.java
@@ -96,6 +96,10 @@ public class TetrisBlock {
         return new TetrisBlock(holder, pattern);
     }
 
+    public void justMove() {
+        realPos.transform(0,0);
+    }
+
     //Move, with no constraints
     public void move(Vector2I delta) {
         for (int i = 0; i < area.length; i++) {
